e78bd1048207c2145a95b5017436342bd6ad161f
Jędrzej Jasiniecki <jedrzej.jasiniecki@gmail.com>
2026-05-12 22:42:06 +0200

    donno

--- src/dataset_collector.py
+++ src/dataset_collector.py
@@ -1,9 +1,10 @@
 import cv2
 import os
 
-cameraIp="http://192.168.1.32:8080/video"
+cameraIp = "http://192.168.1.32:8080/video"
 MODEL_PATH = "../models/face_detection_yunet_2023mar.onnx"
 SAVE_FOLDER = "../dataset/raw"
+
 
 def main():
     os.makedirs(SAVE_FOLDER, exist_ok=True)
@@ -14,26 +15,33 @@ def main():
         input_size=(320, 320),
         score_threshold=0.9
     )
-    cap = cv2.VideoCapture(cameraIp)
+
+    cap = cv2.VideoCapture(0)
     if not cap.isOpened():
+        print("Camera is not working")
         return
 
     w_frame = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h_frame = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     detector.setInputSize((w_frame, h_frame))
 
-    photo_id=0
+    photo_id = 0
     while True:
         ret, frame = cap.read()
         if not ret:
             break
 
         display_frame = frame.copy()
+        x_frame,y_frame=frame.shape[:2]
         _, faces = detector.detect(frame)
-        for face in faces:
-            box = face[:4].astype(int)
-            x, y, w, h = box
-            cv2.rectangle(display_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if faces is not None:
+            for face in faces:
+                box = face[:4].astype(int)
+                x, y, w, h = box
+
+                confidence = face[14]
+                cv2.rectangle(display_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(display_frame, f"{confidence:.2f}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
         cv2.imshow("Akwizycja Danych (Wersja z YuNet)", display_frame)
         key = cv2.waitKey(1) & 0xFF
         if key == 32:
@@ -44,5 +52,7 @@ def main():
             break
     cap.release()
     cv2.destroyAllWindows()
+
+
 if __name__ == "__main__":
     main()
